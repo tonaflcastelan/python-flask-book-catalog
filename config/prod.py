1f264168f2de33bbcfb009709eeb0f2f63dcf903
tonacastelan <tonatiuh.flores@konfio.mx>
2019-10-08 18:51:32 -0500

    made corrections

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,4 @@
 DEBUG = False
 SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:konfio448@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        if not User.query.filter_by(user_name='tona').first():
-            User.create_user(user='tona', email='tona@gmail.com', password='secret')
-    flask_app.run()
+
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    if not User.query.filter_by(user_name='tona').first():
+        User.create_user(user='tona', email='tona@gmail.com', password='secret')
+        flask_app.run()
