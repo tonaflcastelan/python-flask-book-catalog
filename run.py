db6c14466edbc04122454fbaa03805586835875e
tonacastelan <tonatiuh.flores@konfio.mx>
2019-10-08 19:11:53 -0500

    fixed errors

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from app import create_app, db
 from app.auth.models import User
-
+from sqlalchemy import exc
 
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='tona').first():
-        User.create_user(user='tona', email='tona@gmail.com', password='secret')
+    try:
+        if not User.query.filter_by(user_name='tona').first():
+            User.create_user(user='tona', email='tona@gmail.com', password='secret')
+    except exc.IntegrityError:
         flask_app.run()
